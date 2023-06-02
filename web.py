3ab75ffc93dc5570b0e0d0a7acd151be961874b7
SuperGluedGIT <sparkn@mac.com>
2023-06-02 11:54:15 -0400

    Initial commit

---
+++ web.py
@@ -0,0 +1,29 @@
+import streamlit as st  # streamlit run web.py from terminal
+import functions
+
+todos = functions.get_todos()
+
+
+def add_todo():
+    todo = st.session_state["new_todo"] + "\n"
+    todos.append(todo)
+    functions.write_todos(todos)
+    st.session_state["new_todo"] = ""
+
+
+st.title("My Todo App")
+st.subheader("This is my todo app.")
+st.write("This app is to increase your productivity.")
+
+for index, todo in enumerate(todos):
+    checkbox = st.checkbox(todo, key=todo)
+    if checkbox:
+        todos.pop(index)
+        functions.write_todos(todos)
+        del st.session_state[todo]
+        st.experimental_rerun()
+
+st.text_input(label="Add new todo", placeholder="Add new todo...", label_visibility="hidden",
+              on_change=add_todo, key='new_todo')
+
+st.session_state  # prints the session state to the web page
